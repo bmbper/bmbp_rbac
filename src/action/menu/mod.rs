94b225b1d6373e1203b3a88a6163fc8947c5a904
zhangguokai <zhangguokai@inspur.com>
2024-10-09 20:26:22 +0800

    feat: menu

---
+++ src/action/menu/mod.rs
@@ -0,0 +1,4 @@
+mod action;
+mod bean;
+mod service;
+pub use action::*;
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use action::menu::*;
 use action::organ::*;
 use action::role::*;
 use init::build_bmbp_static_router;
@@ -50,14 +51,32 @@ pub fn build_bmbp_rbac_router() -> Router {
                     .push(Router::with_path("batch/remove").post(role_batch_remove))
                     .push(Router::with_path("update/parent").post(role_update_parent))
                     .push(Router::with_path("index.view").get(role_view)),
+            )
+            .push(
+                Router::with_path("menu")
+                    .push(Router::with_path("tree").post(menu_find_tree))
+                    .push(Router::with_path("page").post(menu_find_page))
+                    .push(Router::with_path("list").post(menu_find_list))
+                    .push(Router::with_path("info").post(menu_find_info))
+                    .push(Router::with_path("tree/ignore").post(menu_find_tree_ignore))
+                    .push(Router::with_path("save").post(menu_save))
+                    .push(Router::with_path("insert").post(menu_insert))
+                    .push(Router::with_path("update").post(menu_update))
+                    .push(Router::with_path("enable").post(menu_enable))
+                    .push(Router::with_path("batch/enable").post(menu_batch_enable))
+                    .push(Router::with_path("disable").post(menu_disable))
+                    .push(Router::with_path("batch/disable").post(menu_batch_disable))
+                    .push(Router::with_path("remove").post(menu_remove))
+                    .push(Router::with_path("batch/remove").post(menu_batch_remove))
+                    .push(Router::with_path("update/parent").post(menu_update_parent))
+                    .push(Router::with_path("index.view").get(menu_view)),
             ),
     );
     router = router.push(action_router);
     // view router
     let view_router = Router::with_path("rbac")
         .push(Router::with_path("user.view").get(user_view))
-        .push(Router::with_path("app.view").get(app_view))
-        .push(Router::with_path("menu.view").get(menu_view));
+        .push(Router::with_path("app.view").get(app_view));
 
     router = router.push(view_router);
     return router;
